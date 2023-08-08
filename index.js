0317a04185162daf47a153876963d2564aac4790
bruceliu <bruceliu1107@gmail.com>
2023-08-08 20:30:29 +0800

    fix: headers misspell causing whole app fail

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv').config();
 const port = process.env.PORT || 5001;
+var bodyParser = require("body-parser");
 
 const app = express();
 
 // Enable body parser
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }))
+app.use(bodyParser.urlencoded({ extended: false }));
 
+// set static folder 
 app.use(express.static(path.join(__dirname, 'public')))
-
 app.use('/openai', require('./routes/openaiRoutes'))
 
 app.listen(port, () => console.log("Server started on port " + port));
